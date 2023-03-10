5eb91fbd800f78d2e198353a18760bc5b6d98603
Runatorium <stefano.alesci1@gmail.com>
2023-03-10 16:19:25 +0100

    fix with deconstruction snack 1

--- js/main.js
+++ js/main.js
@@ -8,7 +8,29 @@ let biciclette = [
         'peso' : '5,3'
     },
 ]
+let pesocheck = 1000;
+let bicipiuleggera;
+let nomebici;
 
+biciclette.forEach((elemento) => {
+    const {nome,peso} = elemento;
+    if(peso < pesocheck){
+        bicipiuleggera = peso;
+        pesocheck = peso;
+        nomebici = nome;
+    }
+});
+
+console.log(`${bicipiuleggera} ${nomebici}`);
+
+
+
+
+    
+
+
+
+/*
 let lightbike = calcolo();
 
 
@@ -21,4 +43,4 @@ function calcolo (){
  else{
     return biciclette[1].peso;
  }
-} 
+} */
