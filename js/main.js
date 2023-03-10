12ab422e3bf6c68ddc718e187d5dcb82bd619754
Runatorium <stefano.alesci1@gmail.com>
2023-03-10 16:19:45 +0100

    snack 1 fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 let biciclette = [
     {
         'nome' : 'mountainbike',
-        'peso' : '4,5'
+        'peso' : 4.5
     },
     {
         'nome' : 'BMX',
-        'peso' : '5,3'
+        'peso' : 5.3
     },
 ]
-let pesocheck = 1000;
-let bicipiuleggera;
-let nomebici;
+    let pesocheck = 1000;
+    let bicipiuleggera;
+    let nomebici;
 
 biciclette.forEach((elemento) => {
     const {nome,peso} = elemento;
@@ -25,22 +25,3 @@ console.log(`${bicipiuleggera} ${nomebici}`);
 
 
 
-
-    
-
-
-
-/*
-let lightbike = calcolo();
-
-
-console.log(lightbike);
-
-
-function calcolo (){
- if(biciclette[0].peso < biciclette[1].peso )
- return biciclette[0].peso;
- else{
-    return biciclette[1].peso;
- }
-} */
